7ab1d3d34f05999c2b721a097547382be4d317cb
Ahmad <a.maarouf@turnsol.tech>
2022-09-16 15:24:43 +0300

    add category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table="categories";
+    protected $fillable=[
+        'name',
+        'slug',
+        'status',
+        'meta_keyword',
+        'meta_description',
+        'meta_title',
+        'image',
+        'description',
+        
+    ];
 }
--- database/migrations/2022_09_16_115832_create_categories_table.php
+++ database/migrations/2022_09_16_115832_create_categories_table.php
@@ -16,6 +16,13 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->boolean('status')->default(0)->comment('0=active,1=notactive');
+            $table->longText('description');
+            $table->string('slug');
+            $table->string('image');
+            $table->string('meta_title');
+            $table->string('meta_keyword');
+            $table->mediumText('meta_description');
             $table->timestamps();
         });
     }
